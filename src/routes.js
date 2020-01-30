1cf6a5e622477c32b9f35924f2543b2ab90d40fc
Luciano Sobral <sobral.luciano@gmail.com>
2020-01-30 19:59:15 -0400

    post route for SessionController store

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController'
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
@@ -9,5 +10,9 @@ routes.get('/users', UserController.index);
 routes.get('/users/:id', UserController.show);
 routes.put('/users/:id', UserController.update);
 routes.delete('/users/:id', UserController.delete);
+
+
+//Session
+routes.post('/sessions', SessionController.store);
 
 export default routes;
