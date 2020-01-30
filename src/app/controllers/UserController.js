a7b84dd4b158c8ab7c9d5854ce0614ef6c26406a
Luciano Sobral <sobral.luciano@gmail.com>
2020-01-30 11:44:28 -0400

    Add: UserController has been implemented

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,23 @@
+import User from '../models/User';
+
+export default {
+  async store (request, response) {
+      const {name, email, password, provider} = request.body;
+
+      let user = await User.findOne({ where: { email } });
+
+      if(!user){
+
+        user = await User.create({
+            name,
+            email,
+            password_hash: password,
+            provider
+        });
+
+      }
+
+      return response.json(user);
+  },
+
+};
