b5d04423a509e610778e86badef98be4530efdd0
Luciano Sobral <sobral.luciano@gmail.com>
2020-01-31 11:10:53 -0400

    Organizing precedence in routes

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,20 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController'
 import SessionController from './app/controllers/SessionController';
-
+import authMiddleware from './app/middlewares/auth';
 const routes = new Router();
 
-/// User
+routes.post('/sessions', SessionController.store);
 routes.post('/users', UserController.store);
+
+routes.use(authMiddleware);
+
 routes.get('/users', UserController.index);
 routes.get('/users/:id', UserController.show);
-routes.put('/users/:id', UserController.update);
+routes.put('/users/',UserController.update);
 routes.delete('/users/:id', UserController.delete);
 
 
 //Session
-routes.post('/sessions', SessionController.store);
 
 export default routes;
