99830908ed63cb96adb3bab3a467230e581d0285
Luciano Sobral <sobral.luciano@gmail.com>
2020-02-06 15:00:45 -0400

    sentry - Setting sentry commands in application

--- src/app.js
+++ src/app.js
@@ -1,21 +1,37 @@
+import * as Sentry from '@sentry/node';
 import express, { json } from 'express';
+import 'express-async-errors';
+import Youch from 'youch';
 import routes from './routes';
-
+import SentryConfig from './config/sentry';
 import './database';
 
 class App {
   constructor() {
     this.server = express();
+    Sentry.init(SentryConfig);
+
     this.middleware();
     this.routes();
   }
 
   middleware() {
+    this.server.use(this.onError);
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(json());
   }
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  async onError(err, req, res, next) {
+    const errors = await Youch(err, req).toJSON();
+
+    res.statusCode = 500;
+    return res.json(errors);
+    // res.end(`${res.sentry}\n`);
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://827468ca6d6645feb8562332d0e889bd@sentry.io/2301986',
+};
