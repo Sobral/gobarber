4fa5ccf825988a455131ecb665978cfba4fcc44d
Luciano Sobral <sobral.luciano@gmail.com>
2020-02-11 14:16:40 -0400

    feat(list providers) Users that are providers can be list on /providers
    Now we can list providers send a GET method to /providers resource, the
    response is a list of Users who are providers.
    
    - List providers, Implement /providers

--- src/app/controllers/ProviderController.js
+++ src/app/controllers/ProviderController.js
@@ -1,4 +1,5 @@
 import User from '../models/User';
+import File from '../models/File';
 
 class ProviderController {
   async index(request, response) {
@@ -6,7 +7,12 @@ class ProviderController {
       where: {
         provider: true,
       },
-      attributes: ['id', 'name', 'email', 'provider'],
+      attributes: ['id', 'name', 'email', 'avatar_id'],
+      include: {
+        model: File,
+        as: 'avatar',
+        attributes: ['path', 'name'],
+      },
     });
 
     return response.status(200).json(users);
