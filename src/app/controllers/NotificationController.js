a7ad6f8acd33f2b04fbc8f41c5e8e0c4af6d51f6
Luciano Sobral <sobral.luciano@gmail.com>
2020-02-14 15:10:53 -0400

    improv(Notifications) Provider can mark a notification as read

--- src/app/controllers/NotificationController.js
+++ src/app/controllers/NotificationController.js
@@ -9,6 +9,18 @@ class NotificationController {
       .limit(20);
     return response.status(200).json(notification);
   }
+
+  async update(request, response) {
+    const { id } = request.params;
+
+    const notification = await Notification.findByIdAndUpdate(
+      id,
+      { read: true },
+      { new: true }
+    );
+
+    return response.json(notification);
+  }
 }
 
 export default new NotificationController();
--- src/routes.js
+++ src/routes.js
@@ -34,4 +34,6 @@ routes.get('/schedules', ScheduleController.index);
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
+
 export default routes;
