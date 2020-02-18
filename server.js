b0c91e27255f03414f824945bcc5f8b40d7afbe3
Luciano Sobral <sobral.luciano@gmail.com>
2020-02-18 15:40:31 -0400

    Attempt make heroku run properly 2

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
+const express = require('express');
+const path = require('path');
+const serveStatic = require('serve-static');
 
-var app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
+const app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
 
-var port = process.env.PORT || 8000
-app.listen(port)
-console.log('server started ' + port)
+const port = process.env.PORT || 8000;
+app.listen(port);
+console.log(`server started ${port}`);
